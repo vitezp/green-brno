0019843ebf18ed974a244ff7d43b58a1ec897bc2
Štefan Matta <stefanmatta@yahoo.com>
2022-04-08 21:37:57 +0200

    Fix text/plain for count query

--- data-service-example/src/main/java/org/cobolaci/hackaton/greenbrno/config/DataServiceExampleConfig.java
+++ data-service-example/src/main/java/org/cobolaci/hackaton/greenbrno/config/DataServiceExampleConfig.java
@@ -1,26 +1,39 @@
 package org.cobolaci.hackaton.greenbrno.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.util.MimeTypeUtils.TEXT_PLAIN;
 
 @Data
 @ConfigurationProperties("org.cobolaci.hackaton.greenbrno")
 public class DataServiceExampleConfig {
 
+    private static final int BUFFER_SIZE_16MB = 16 * 1000 * 1000;
+
     private DataProviders providers;
-    private DataSources dataPoints;
+    private DataSources dataSources;
 
     @Bean
     public WebClient acgisWebClient() {
         return WebClient.builder()
-                .exchangeStrategies(ExchangeStrategies.builder()
-                        .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16000000))
-                        .build())
+                .exchangeStrategies(ExchangeStrategies.builder().codecs(this::acceptedCodecs).build())
                 .baseUrl(providers.getArcgis().getBaseUrl())
                 .build();
+    }
+
+    private void acceptedCodecs(ClientCodecConfigurer clientCodecConfigurer) {
+        clientCodecConfigurer.defaultCodecs().maxInMemorySize(BUFFER_SIZE_16MB);
+        // api returns text/plain instead of application/json for count request
+        clientCodecConfigurer.customCodecs().registerWithDefaultConfig(new Jackson2JsonDecoder(new ObjectMapper(), TEXT_PLAIN));
+        clientCodecConfigurer.customCodecs().registerWithDefaultConfig(new Jackson2JsonEncoder(new ObjectMapper(), TEXT_PLAIN));
     }
 
     @Bean
