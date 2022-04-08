a129181d0d7fdba779d320373bc2c267d9f5df28
Lubomir Gocnik <gocnikl@gmail.com>
2022-04-08 20:17:48 +0200

    random commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
-import { PriceRangeAnimation } from './components/PriceRangeAnimation/PriceRangeAnimation'
+// import { PriceRangeAnimation } from './components/PriceRangeAnimation/PriceRangeAnimation'
+import { Map } from './map/components/Map'
 
 function App() {
   return (
     <div className="App">
-      <PriceRangeAnimation from={430} to={7500} />
+      {/*<PriceRangeAnimation from={430} to={7500} />*/}
+      <Map />
     </div>
   )
 }
