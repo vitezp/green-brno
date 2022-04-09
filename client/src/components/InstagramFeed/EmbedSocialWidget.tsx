d4d926aa097acabe4af688f0dc4f17e4cc43c975
Lubomir Gocnik <gocnikl@gmail.com>
2022-04-09 21:28:53 +0200

    fixed

--- client/src/components/InstagramFeed/EmbedSocialWidget.tsx
+++ client/src/components/InstagramFeed/EmbedSocialWidget.tsx
@@ -1,25 +1,20 @@
-import React, { Component } from "react";
+import React, { useEffect } from 'react'
 
-export default class EmbedSocialWidget extends Component {
-    render() {
-        const props: any = {
-            className:"embedsocial-hashtag", dataref:this.props.refId
-        }
-        return (
-            <div{ ...props}></div>
-        );
+export const EmbedSocialWidget = ({ refId }: any) => {
+  useEffect(() => {
+    const scriptik = (d: any, s: any, id: any) => {
+      const js: any = d.createElement(s)
+      js.id = id
+      js.src = 'https://embedsocial.com/cdn/ht.js'
+      d.getElementsByTagName('head')[0].appendChild(js)
     }
 
-    componentDidMount() {
-        (function (d, s, id) {
-            var js;
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = "https://embedsocial.com/embedscript/ri.js";
-            d.getElementsByTagName("head")[0].appendChild(js);
-        })(document, "script", "EmbedSocialReviewScript");
-    }
+    scriptik(document, 'script', 'EmbedSocialHashtagScript')
+  }, [])
+
+  return (
+    <>
+      <div className="embedsocial-hashtag" data-ref={refId}></div>
+    </>
+  )
 }
