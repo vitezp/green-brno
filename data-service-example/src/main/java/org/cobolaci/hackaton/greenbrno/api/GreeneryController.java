df42f8d1374f0893d215a28b45810392b1b76493
Štefan Matta <stefanmatta@yahoo.com>
2022-04-09 10:12:06 +0200

    Greenery Controller - v1

--- data-service-example/src/main/java/org/cobolaci/hackaton/greenbrno/api/GreeneryController.java
+++ data-service-example/src/main/java/org/cobolaci/hackaton/greenbrno/api/GreeneryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/greenery")
+@RequestMapping("/api/v1/greenery")
 @RestController
 public class GreeneryController {
 
