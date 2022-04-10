4145a7093452f7a9d979eb8ae13effe25a2117fa
Dominik Hanak <dhanak@redhat.com>
2022-04-10 03:56:22 +0200

    Implement street light map with LED icons

---
+++ client/src/streetLightsMap/components/LightsMap/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useEffect, useRef } from 'react'
+import mapboxgl from 'mapbox-gl'
+import { onLoad } from '../../handlers/onLoad'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+export const LightsMap: FC = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null)
+
+  // initialize map
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibHVib2dvY28iLCJhIjoiY2t1eHc4ZWR2MDY5MjMzbGRhdjJrdGxlbSJ9.cKiGo38vzuZ-cLoL_UU09w'
+
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current!,
+      style: 'mapbox://styles/mapbox/light-v10',
+      center: [16.6035456, 49.2044288],
+      zoom: 12,
+      maxBounds: new mapboxgl.LngLatBounds(
+        new mapboxgl.LngLat(16.251, 49.0929),
+        new mapboxgl.LngLat(16.97222, 49.3118)
+      ),
+    })
+
+    // add navigation control (zooming and rotating)
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+
+    map.on('load', onLoad({ map }))
+
+    // Clean up on unmount
+    return () => {
+      map.remove()
+    }
+  }, [])
+
+  return <div style={{ width: '100vw', height: '100vh' }} ref={mapContainerRef} />
+}
---
+++ client/src/streetLightsMap/data/index.ts
@@ -0,0 +1,6 @@
+export const STREET_LIGHTS_JSON_URL = '/data/street-lights.geojson'
+export const STREET_LIGHTS_SOURCE_ID_LED = 'street-lights-source-led'
+export const STREET_LIGHTS_LAYER_ID_LED = 'street-lights-layer-led'
+
+export const STREET_LIGHTS_SOURCE_ID = 'street-lights-source'
+export const STREET_LIGHTS_LAYER_ID = 'street-lights-layer'
